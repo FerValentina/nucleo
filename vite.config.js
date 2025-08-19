6f7b704e5e5f498c16b0e85770e7ed4e51c2b162
Fernanda <fvalentina.gf@gmail.com>
2025-08-19 08:52:45 -0400

    dist

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,3 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   base: '/hito7/',
-//   plugins: [react()],
-// })
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
@@ -14,3 +5,4 @@ export default defineConfig({
   base: '/proyect/', // Cambia a '/proyect/' para tu repositorio
   plugins: [react()],
 });
+
