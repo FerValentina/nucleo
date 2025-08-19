1d8fc400f55a44a6ac6781b05bbdd2dc91c7426b
Fernanda <fvalentina.gf@gmail.com>
2025-08-19 10:25:33 -0400

    arreglado

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/proyect/', // Cambia a '/proyect/' para tu repositorio
+  base: '/proyect/', // Confirma que es '/proyect/'
   plugins: [react()],
 });
-
