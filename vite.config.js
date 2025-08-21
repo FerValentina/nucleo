705dee4fe32555ddda236d1731a714ec60ac081d
Fernanda <fvalentina.gf@gmail.com>
2025-08-20 23:32:25 -0400

    nucleo

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/proyect', // Confirma que es '/proyect/'
+  base: '/nucleo', // Confirma que es '/proyect/'
   plugins: [react()],
 });
